5c36f9007e8e5f569a31dbf7ca23f0f49a0aadb6
MADHUSUDHANPATI <patimadhusudhan7@gmail.com>
2025-12-19 12:29:08 +0530

    largest_number

---
+++ Largest_Number_optimal.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int Largest_Number( int arr[], int n) {
+
+    // int n= arr.size();             // arr is a plain array (int arr[]) Plain arrays do NOT have .size()
+    int largest = arr[0];
+    for(int i=1;i<n;i++) {
+        if(arr[i] > largest) {
+            largest= arr[i];
+        }
+    }
+    return largest;
+}
+int main() {
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for( int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+
+    cout <<"Largest number is : " <<  Largest_Number( arr, n);
+}
---
+++ Second_Largest_Number_Better.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Second_Largest_Number( int arr[], int n) {
+
+    int largest = arr[0];
+    int secondLargest=-1;
+    for( int i=1;i<n;i++) {
+        if( arr[i] > largest)  {
+            largest=arr[i];
+        }
+    }
+    // cout << largest << ;
+
+    for(int i=0;i<n;i++) {
+        if( arr[i]> secondLargest && arr[i]!=largest) {
+            secondLargest= arr[i];
+        }
+    }
+
+    return secondLargest;
+}
+int main() {
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for( int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+
+    cout <<"Second largest number is :" <<  Second_Largest_Number(arr, n);
+
+}
