68bb8cc6eedf1b33669864c870e50730e0018b32
MADHUSUDHANPATI <patimadhusudhan7@gmail.com>
2026-01-29 00:12:43 +0530

    Leetcode: 1752

---
+++ Check_sorted_rotated.cpp
@@ -0,0 +1,33 @@
+// Leetcode 1752 Problem
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool Check_sorted_rotated(vector<int> & arr) {
+
+    int n= arr.size();
+    int drops=0;
+
+    for( int i=0;i<n;i++) {
+
+        if( arr[i]> arr[(i+1)%n]) drops++;
+    }
+
+    if(drops <=1) return true;
+    return false;
+
+}
+int main() {
+
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+
+    for(  int i=0;i<n;i++) {
+
+        cin >> arr[i];
+    }
+
+    cout << Check_sorted_rotated( arr);
+}
