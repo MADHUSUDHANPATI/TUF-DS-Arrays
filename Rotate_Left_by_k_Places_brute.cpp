cc80a3eca5495b27efa27a269da4a73f47393dec
MADHUSUDHANPATI <patimadhusudhan7@gmail.com>
2026-01-30 00:03:21 +0530

    modified

---
+++ Rotate_Left_by_k_Places_brute.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void Rotate_Left_by_k_Places_brute( vector<int> &nums, int k) {
+
+    int n = nums.size();
+    vector<int> v;
+
+    for( int i=0;i<k ;i++) {
+        v.push_back(nums[i]);
+    }
+
+    for( int i=k ; i <n ; i++) {
+        nums[i-k]= nums[i];
+    }
+
+    for( int i = n-k ; i<n;i++) {
+        nums[i] = v[i-(n-k)];
+    }
+}
+int main() {
+
+    int n, k;
+    cin >> n >> k;
+    vector<int> nums(n);
+
+    for(  int i=0;i<n;i++) {
+
+        cin >> nums[i];
+    }
+
+    Rotate_Left_by_k_Places_brute(nums, k);
+
+    for( int i=0;i<n;i++) {
+        cout << nums[i]<< " ";
+    }
+}
